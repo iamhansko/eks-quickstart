04ed99a9c61d98f1556b98a8fe122d91307cf1ab
iamhansko <iamhansko@kakao.com>
2025-02-12 22:29:01 +0900

    infra: RHEL EKS Optimized AMI Userdata

--- rhel/airgap/README.md
+++ rhel/airgap/README.md
@@ -6,8 +6,8 @@ cd eks-quickstart/rhel/airgap
 chmod +x pre_build_ami.sh build_ami.sh
 
 # Internet Access Enabled
+# Set ENV (https://github.com/aws-samples/amazon-eks-ami-rhel/blob/main/doc/usage/rhel.md)
 ./pre_build_ami.sh
-
 
 # Airgap
 # Set ENV (https://github.com/aws-samples/amazon-eks-ami-rhel/blob/main/doc/usage/rhel.md)
--- rhel/userdata.sh
+++ rhel/userdata.sh
@@ -1,22 +1,37 @@
+MIME-Version: 1.0
+Content-Type: multipart/mixed; boundary="//"
+
+--//
+Content-Type: application/node.eks.aws
+
+---
+apiVersion: node.eks.aws/v1alpha1
+kind: NodeConfig
+spec:
+  cluster:
+    name: # cluster name
+    apiServerEndpoint: # api server endpoint (ex https://XXXXXXXXXXXXX.XXX.XXXXXXXXXX.eks.amazonaws.com)
+    certificateAuthority: # cluster certificate authority
+    cidr: # service ipv4 range (ex 172.20.0.0/16) 
+  kubelet:
+    config:
+      shutdownGracePeriod: 30s
+      featureGates:
+        DisableKubeletCloudCredentialProviders: true
+  containerd:
+    config: |
+      [plugins."io.containerd.grpc.v1.cri".containerd]
+      discard_unpacked_layers = false
+
+--//
+Content-Type: text/x-shellscript; charset="us-ascii"
+
 #!/bin/bash
 
-# https://docs.aws.amazon.com/ko_kr/AWSEC2/latest/UserGuide/ec2-instance-connect-set-up.html#ec2-instance-connect-install
-mkdir /tmp/ec2-instance-connect
-curl https://amazon-ec2-instance-connect-us-west-2.s3.us-west-2.amazonaws.com/latest/linux_amd64/ec2-instance-connect.rhel8.rpm -o /tmp/ec2-instance-connect/ec2-instance-connect.rpm
-curl https://amazon-ec2-instance-connect-us-west-2.s3.us-west-2.amazonaws.com/latest/linux_amd64/ec2-instance-connect-selinux.noarch.rpm -o /tmp/ec2-instance-connect/ec2-instance-connect-selinux.rpm
-sudo yum install -y /tmp/ec2-instance-connect/ec2-instance-connect.rpm /tmp/ec2-instance-connect/ec2-instance-connect-selinux.rpm
+# https://github.com/aws-samples/amazon-eks-ami-rhel/issues/2
+systemctl stop nm-cloud-setup.timer
+systemctl disable nm-cloud-setup.timer
+systemctl stop nm-cloud-setup.service
+systemctl disable nm-cloud-setup.service
 
-# VS Code Server
-export HOME=/home/ec2-user
-curl -fsSL https://code-server.dev/install.sh | sh
-mkdir -p /home/ec2-user/.config/code-server
-cat <<EOF > /home/ec2-user/.config/code-server/config.yaml
-bind-addr: 0.0.0.0:8000
-auth: none
-cert: false
-EOF
-chown -R ec2-user:ec2-user /home/ec2-user/.config
-mkdir -p /etc/systemd/system/code-server@.service.d
-echo -e "[Service]\nExecStart=\nExecStart=/usr/bin/code-server /home/ec2-user" > /etc/systemd/system/code-server@.service.d/override.conf
-systemctl daemon-reload
-systemctl enable --now code-server@ec2-user
+--//--
