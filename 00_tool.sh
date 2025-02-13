7b7705970e83d076c8e12c0b043508dcd8d2ff68
iamhansko <iamhansko@kakao.com>
2025-02-14 04:37:25 +0900

    infra: kubectl, helm, eksctl Installation

--- 00_tool.sh
+++ 00_tool.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-# export aws_region=""
-# export cluster_name=""
-source /home/ec2-user/.bashrc
-
-cd /home/ec2-user
 mkdir -p /home/ec2-user/bin
 curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.30.0/2024-05-12/bin/linux/amd64/kubectl
 chmod +x kubectl
@@ -14,13 +9,8 @@ echo "alias k=kubectl" >>~/.bashrc
 echo "complete -o default -F __start_kubectl k" >>~/.bashrc
 echo "source <(kubectl completion bash)" >>~/.bashrc
 
-aws eks update-kubeconfig --region us-east-1 --name test
-# aws eks update-kubeconfig --region ${AWS::Region} --name ${EksCluster}
-
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 sudo mv /tmp/eksctl /usr/local/bin
-aws configure set region us-east-1
-# aws configure set region ${AWS::Region}
 
 curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 > /home/ec2-user/get_helm.sh
 chmod 700 /home/ec2-user/get_helm.sh
--- env.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo "aws_region='us-east-1'" >>/home/ec2-user/.bashrc
-echo "cluster_name='my-cluster'" >>/home/ec2-user/.bashrc
