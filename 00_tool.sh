0c1f15bcf451c24442c7fa0f527acd69936c4052
iamhansko <iamhansko@kakao.com>
2025-04-27 23:55:33 +0900

    infra: Install kubectl & eksctl & helm

--- 00_tool.sh
+++ 00_tool.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
+set -o xtrace
+
+export HOME="/home/ec2-user"
 mkdir -p /home/ec2-user/bin
-curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.30.0/2024-05-12/bin/linux/amd64/kubectl
+# https://docs.aws.amazon.com/ko_kr/eks/latest/userguide/install-kubectl.html#kubectl-install-update
+curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.32.0/2024-12-20/bin/linux/amd64/kubectl
+# curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.31.3/2024-12-12/bin/linux/amd64/kubectl
+# curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.30.7/2024-12-12/bin/linux/amd64/kubectl
+# curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.29.10/2024-12-12/bin/linux/amd64/kubectl
+# curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.28.15/2024-12-12/bin/linux/amd64/kubectl
 chmod +x kubectl
 mv kubectl /home/ec2-user/bin/kubectl
 export PATH=/home/ec2-user/bin:$PATH
@@ -16,4 +24,4 @@ curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 > /ho
 chmod 700 /home/ec2-user/get_helm.sh
 /home/ec2-user/get_helm.sh
 
-exec bash
+source ~/.bashrc
